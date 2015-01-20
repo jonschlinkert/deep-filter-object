b0c1bc975a8de47687199a3d5895b6f94c892dff
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-19 22:39:32 -0500

    optimizations, pass options to filter-object

--- index.js
+++ index.js
@@ -3,27 +3,33 @@
 var filterObject = require('filter-object');
 var isObject = require('isobject');
 
-module.exports = function deepFilter(obj, patterns) {
+/**
+ * Expose `deepFilter`
+ */
+
+module.exports = deepFilter;
+
+
+function deepFilter(obj, patterns, options, i) {
   if (obj == null) {
     throw new Error('deep-filter-object expects an object');
   }
-  if (arguments.length === 1) {
-    return obj;
-  }
 
-  obj = filterObject(obj, patterns);
-  var o = {};
+  if (arguments.length === 1) return obj;
+
+  obj = filterObject(obj, patterns, options);
+  var res = {};
 
   for (var key in obj) {
     if (obj.hasOwnProperty(key)) {
       var value = obj[key];
 
       if (isObject(value)) {
-        o[key] = deepFilter(value, patterns);
+        res[key] = deepFilter(value, patterns, options);
       } else {
-        o[key] = value;
+        res[key] = value;
       }
     }
   }
-  return o;
+  return res;
 };
